2e19335781d5630443374d0a8b3c9f63e3589bb3
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-11-30 11:06:42 -0800

    Create string_fun.py

---
+++ StringFun/string_fun.py
@@ -0,0 +1,18 @@
+"""
+Header block goes here
+
+"""
+
+# string_input function
+
+
+# no_vowel function
+
+
+def main():
+  # all of your function calls go here
+
+
+# don't change this
+if __name__ == "__main__":
+	main()
