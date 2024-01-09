b09e10330c6ab243104eac6c867f8f2c0b63d055
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-01-09 09:47:04 -0800

    Create list_fun.py

---
+++ ListFun/list_fun.py
@@ -0,0 +1,39 @@
+# put your header block here
+
+# import statements here if needed
+
+
+# all functions go between this line and main
+
+# get_data prints instructions and gets 10 ints from user and stores them in an array
+
+# print_data prints the values on one line with a space between them
+
+# lowest_value returns lowest value
+
+# highest_value returns the highest value
+
+# count_odds returns the number of odd numbers
+
+# sum returns the sum of the array values
+
+# get_duplicates returns an array of duplicate values
+
+
+
+
+
+
+
+
+# main method - all function calls go in here
+def main():
+    # the original data should not be altered. If you store your data as user_nums for example
+    # I should be able to print user_nums at the end and see the original data in its original
+    # order
+
+    print("Hi") #you can erase this
+
+
+if __name__ == '__main__':
+    main()
